d354d8127f659332f0e0cde7844b480794590727
Maxime Helen <maxime.helen@starbreeze.com>
2016-10-19 18:24:15 -0700

    First stage

---
+++ README.md
@@ -0,0 +1,29 @@
+# Search parser
+
+This is a search parser for query syntax comprising of macros and logic operators. 
+It aims to support what is specified in this [document](https://docs.google.com/document/d/1jca1EKI4C13PzzjfIEMZ9-zPy0Uiv4MH7PjuSHm1pXg/edit)
+
+## Installation
+
+```npm install @sb/search_parser```
+
+## Usage
+
+```
+const search_parser = require("search_parser");
+//macro regular expression to catch specific keywords and eventual operators
+const macro_regex = /^(kind|mime|created|modified|extension|size|depth):(<=?|>=?|\.\.)?$/;
+const query_string = 'test AND query OR "hello word" kind: file';
+
+var query_json = search_parser(query_string, macro_regex);
+```
+
+## Test
+
+```npm test```
+
+Check programms are based on mocha and should npm modules.
+
+## License
+
+The MIT License (MIT)
---
+++ index.js
@@ -0,0 +1,63 @@
+'use strict';
+
+// TO DO write our own parser
+const parser = require('logic-query-parser');
+
+module.exports = (query, macro_regex) => {
+    const binaryTree = parser.parse(query);
+    query = parser.utils.binaryTreeToQueryJson(binaryTree);
+    const parse_macros = values => {
+        values.forEach((element, index) => {
+            if (element.type === "string") {
+                let match = element.value.match(macro_regex);
+                if (match) {
+                    element.type = match[1];
+                    if (match[2]) {
+                        element.operator = match[2];
+                    }
+                    if (values[index + 1]) {
+                        element.value = values[index + 1].value;
+                        values.splice(index + 1, 1);
+                        if (element.operator === "..") {
+                            element.value += element.operator + values[index + 1].value;
+                            values.splice(index + 1, 1);
+                        }
+                    }
+                } else {
+                    element.type = "word";
+                }
+            } else if (element.values) {
+                element.values = parse_macros(element.values);
+                const is_value_a_macro = macro_regex.test(element.values[0].value);
+                const is_type_a_macro = macro_regex.test(element.values[0].type + ":");
+                if (element.values.length === 1 && (is_type_a_macro || is_value_a_macro)) {
+                    const macros = parse_macros(element.values);
+                    if (is_value_a_macro && values[index+1]) {
+                        const new_value = values[index+1].value;
+                        macros[0].value = new_value;
+                        element.values = macros;
+                        values.splice(index+1, 1)
+                        values[index] = element;
+                    } else if (is_type_a_macro && element.type === "and") {
+                        const new_values = element.values[0];
+                        values[index] = new_values;
+                    }
+                }
+            }
+        });
+        return values;
+    };
+    if (query.err) {
+        throw query.err;
+    }
+    if (query.values) {
+        query.values =  parse_macros(query.values);
+        if (query.values.length === 1) {
+            query = query.values[0];
+        }
+    }
+    if (query.type === "string") {
+        query.type = "word";
+    }
+    return query;
+};
