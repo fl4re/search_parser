ecee1e81daec5fb42de6b1fe20734066682d1fd2
Maxime Helen <maxime.helen@starbreeze.com>
2017-04-04 13:23:40 -0700

    Add NOT logic operator in syntax

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const parser = require('logic-query-parser');
 module.exports = (query, macro_regex) => {
     const binaryTree = parser.parse(query);
     query = parser.utils.binaryTreeToQueryJson(binaryTree);
-    const parse_macros = values => {
+    const parse_macros = (values, next) => {
         values.forEach((element, index) => {
             if (element.type === "string") {
                 let match = element.value.match(macro_regex);
@@ -15,19 +15,33 @@ module.exports = (query, macro_regex) => {
                     if (match[2]) {
                         element.operator = match[2];
                     }
-                    element.value = values[index + 1].value;
-                    values.splice(index + 1, 1);
-                    if (element.operator === "..") {
-                        element.value += element.operator + values[index + 1].value;
+                    if (values[index + 1]) {
+                        element.value = values[index + 1].value;
                         values.splice(index + 1, 1);
+                        if (element.operator === "..") {
+                            element.value += element.operator + values[index + 1].value;
+                            values.splice(index + 1, 1);
+                        }
                     }
                 } else {
                     element.type = "word";
                 }
             } else if (element.values) {
                 element.values = parse_macros(element.values);
-                if (element.values.length === 1) {
-                    values[index] = element.values[0];
+                const is_value_a_macro = macro_regex.test(element.values[0].value);
+                const is_type_a_macro = macro_regex.test(element.values[0].type + ":");
+                if (element.values.length === 1 && (is_type_a_macro || is_value_a_macro)) {
+                    const macros = parse_macros(element.values);
+                    if (is_value_a_macro && values[index+1]) {
+                        const new_value = values[index+1].value;
+                        macros[0].value = new_value;
+                        element.values = macros;
+                        values.splice(index+1, 1)
+                        values[index] = element;
+                    } else if (is_type_a_macro && element.type === "and") {
+                        const new_values = element.values[0];
+                        values[index] = new_values;
+                    }
                 }
             }
         });
